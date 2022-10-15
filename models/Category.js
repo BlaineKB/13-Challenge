5d72b45b0eb77bf45248494b59ea3ba5053dee1b
BKB <blainekb94@gmail.com>
2022-10-15 07:39:45 -0500

    added routes and fixed typos in models

--- models/Category.js
+++ models/Category.js
@@ -10,7 +10,7 @@ Category.init(
     id: {
       type: DataTypes.INTEGER,
       allowNull: false,
-      primaryKeu: true,
+      primaryKey: true,
       autoIncrement: true
     },
     category_name: {
@@ -23,7 +23,7 @@ Category.init(
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'category',
+    modelName: 'category'
   }
 );
 
